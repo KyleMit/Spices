4fe8fff32c21e27fa372128bc1393639bc75410e
Kyle <Kyle@KyleMitofsky.com>
2014-03-01 02:00:18 -0500

    Added Navbar with Dynamic Collapse
    Blogged about implementation here:
    http://www.codingeverything.com/2014/02/BootstrapDocsSideBar.html

--- Scripts/Site.js
+++ Scripts/Site.js
@@ -5,50 +5,11 @@
 
     $(function () {
         
-        // when menu item clicked
-        $("a[href^='#']").click(function () {
-            // scroll to position
-            navigateToElement(this);
-        });
 
-        $(".MobileToggle a").click(function () {
-            var viewport = ($(this).attr('id') === "MobileSite") ?
-                            'width=device-width, initial-scale=1.0' :
-                            'width=1000';
-            $("meta[name=viewport]").attr('content', viewport);
-            $(".MobileToggle").toggle();
-            return false;
-        });
 
-        $("#ViewResume").click(function() {
-            //showPdfDialog();
-        });
     });
 
-    function navigateToElement(pageLink) {
-        // get href attribute
-        var idSelector = $(pageLink).attr('href');
-        // make sure we have a valid id
-        if ($(idSelector).length > 0) {
-            // scroll down page
-            scrollToElement(idSelector);
-            // reset url hash
-            setHash(idSelector);
-            // return false so we don't refresh
-            //return false;    
-        }
-    }
-
-    function scrollToElement(idSelector) {
-        $('html, body').animate({
-            scrollTop: $(idSelector).offset().top
-        }, 1000);
-    }
-
-    function setHash(hash) {
-        window.location.hash = hash;
-    }
-
+    
 }());
 
 function InitializePage() {
@@ -65,6 +26,13 @@ function InitializePage() {
         template = getTemplate("templates\\template.html", "#nav-listing");
         output = template(context);
         $("#NavPlaceholder").append(output);
+
+        $('body').scrollspy({
+            target: '#sidebar',
+            offset: 70
+        });
+        
+        scrollToElement(window.location.hash);
     });
 
 }
@@ -125,4 +93,10 @@ function getTemplate(path, selector) {
     });
     
     return template;
+}
+
+function scrollToElement(idSelector) {
+    $('html, body').animate({
+        scrollTop: $(idSelector).offset().top
+    }, 1000);
 }
